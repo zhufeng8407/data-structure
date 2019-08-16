bff0ba7c71d682e0c026eb9908a6adbf6919a95e
zhufeng8407 <zhufeng.zhu@163.com>
2019-08-16 18:06:03 +0800

    linkedlist study

---
+++ src/main/java/com/feng/data/leetcode/LeetCode203.java
@@ -0,0 +1,18 @@
+package com.feng.data.leetcode;
+
+public class LeetCode203 {
+
+
+    public ListNode removeElements(ListNode head, int val) {
+
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}
--- src/main/java/com/feng/data/linkedlist/LinkedList.java
+++ src/main/java/com/feng/data/linkedlist/LinkedList.java
@@ -2,8 +2,36 @@ package com.feng.data.linkedlist;
 
 public class LinkedList<E> {
 
+    private Node head;
+
+    private int size;
+
+    public void addFirst(E e) {
+        Node node = new Node(e);
+        node.next = head.next;
+        head.next = node;
+
+//        head.next = new Node(e, head.next);
+        size++;
+    }
 
     private class Node {
 
+        private E e;
+
+        private Node next;
+
+        public Node(E e, Node node) {
+            this.e = e;
+            next = node;
+        }
+
+        public Node(E e) {
+            this(e, null);
+        }
+
+        public Node() {
+            this(null, null);
+        }
     }
 }
