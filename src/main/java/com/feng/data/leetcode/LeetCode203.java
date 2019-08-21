dc35d14908b17df7cbf2a7edcefeec4685210cde
zhufeng8407 <zhufeng.zhu@163.com>
2019-08-21 15:50:27 +0800

    linkedlist study

--- src/main/java/com/feng/data/leetcode/LeetCode203.java
+++ src/main/java/com/feng/data/leetcode/LeetCode203.java
@@ -4,7 +4,22 @@ public class LeetCode203 {
 
 
     public ListNode removeElements(ListNode head, int val) {
+        ListNode dummyHead = new ListNode(-1);
+        dummyHead.next = head;
 
+        ListNode prev = dummyHead;
+
+        while(prev.next != null) {
+            if (prev.next.val == val) {
+                ListNode delNode = prev.next;
+                prev.next = delNode.next;
+                delNode.next = null;
+            } else {
+                prev = prev.next;
+            }
+        }
+
+        return dummyHead.next;
     }
 
     class ListNode {
