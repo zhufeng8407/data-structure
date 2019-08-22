5a98b76e16b818624e4b27c2ffc00667254eb2fd
zhufeng8407 <zhufeng.zhu@163.com>
2019-08-22 18:03:13 +0800

    tree study

---
+++ src/main/java/com/feng/data/leetcode/LeetCode538.java
@@ -0,0 +1,18 @@
+package com.feng.data.leetcode;
+
+public class LeetCode538 {
+
+    public TreeNode convertBST(TreeNode root) {
+        return null;
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
